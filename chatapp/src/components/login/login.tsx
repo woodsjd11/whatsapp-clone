75cd7de4289b47a4da6364d76a213643baa81b57
woodsjd11 <104032001+woodsjd11@users.noreply.github.com>
2022-09-02 12:02:54 +0100

    add form requirements using formik and yup

--- chatapp/src/components/login/login.tsx
+++ chatapp/src/components/login/login.tsx
@@ -8,45 +8,79 @@ import {
   Input,
   Heading,
 } from "@chakra-ui/react";
+import { useFormik, FormikProps } from "formik";
 import React from "react";
+import * as yup from "yup";
 
 export default function Login() {
+  interface FormValues {
+    username: string;
+    password: string;
+  }
+  const personSchema = yup.object({
+    username: yup
+      .string()
+      .required("Username required!")
+      .min(6, "Username must be >5 characters")
+      .max(20, "Username must be <21 characters"),
+    password: yup
+      .string()
+      .required("Password required!")
+      .min(6, "Password must be >5 characters"),
+  });
+
+  const formik: FormikProps<FormValues> = useFormik<FormValues>({
+    initialValues: { username: "", password: "" },
+    validationSchema: personSchema,
+    onSubmit: (values, actions) => {
+      alert(JSON.stringify(values, null, 2));
+      actions.resetForm();
+    },
+  });
+
   return (
-      <VStack
-      as="form"
+    <VStack
       w={{ base: "90%", md: "500px" }}
       m="auto"
       justify="center"
       h="100vh"
       spacing="1rem"
-      >
-        <Heading>Log In</Heading>
-      <FormControl>
-        <FormLabel fontSize="lg">Username</FormLabel>
-        <Input
-          name="username"
-          placeholder="Enter Username"
-          autoComplete="off"
-          fontSize="lg"
-        ></Input>
-        <FormErrorMessage>Invalid Username</FormErrorMessage>
-      </FormControl>
-      <FormControl>
-        <FormLabel fontSize="lg">Password</FormLabel>
-        <Input
-          name="password"
-          placeholder="Enter Password"
-          autoComplete="off"
-          fontSize="lg"
-        ></Input>
-        <FormErrorMessage>Invalid Password</FormErrorMessage>
-      </FormControl>
-      <ButtonGroup pt="1rem">
-        <Button colorScheme="teal" type="submit">
-          Log In
-        </Button>
-        <Button>Sign Up</Button>
-      </ButtonGroup>
+    >
+      <Heading>Log In</Heading>
+      <form onSubmit={formik.handleSubmit}>
+        <FormControl>
+          <FormLabel fontSize="lg">Username</FormLabel>
+          <Input
+            name="username"
+            placeholder="Enter Username"
+            autoComplete="off"
+            fontSize="lg"
+            value={formik.values.username}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          ></Input>
+          <FormErrorMessage>Invalid Username</FormErrorMessage>
+        </FormControl>
+        <FormControl>
+          <FormLabel fontSize="lg">Password</FormLabel>
+          <Input
+            name="password"
+            placeholder="Enter Password"
+            autoComplete="off"
+            fontSize="lg"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          ></Input>
+          <FormErrorMessage>Invalid Password</FormErrorMessage>
+        </FormControl>
+        <ButtonGroup pt="1rem">
+          <Button colorScheme="teal" type="submit">
+            Log In
+          </Button>
+          <Button>Sign Up</Button>
+        </ButtonGroup>
+      </form>
     </VStack>
   );
 }
